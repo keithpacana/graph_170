27f747d2e944679e5f438a53826fb5179b094797
Keith Pacana <keithpacana@berkeley.edu>
2018-04-27 16:00:31 -0700

    ok

--- solver_for_hive1.py
+++ solver_for_hive1.py
@@ -1,5 +1,5 @@
 from tsp_aws_solver import solver
 
 
-solver("./curr_hive1.txt", "./iter_hive1.txt", "./beaten_hive1.txt", "./out/", False, 0, 50)
+solver("./curr_hive1.txt", "./iter_hive1.txt", "./beaten_hive1.txt", "./out/", False, 0, 10)
 #file write to what current file it is on, iteration it's on, beaten and value, where it's writing to
