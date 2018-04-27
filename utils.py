bc4c94c53d4fd537811733add9bb314b4173176e
Chris Sreesangkom <csreesan@berkeley.edu>
2018-04-26 22:39:31 -0700

    uitls

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import sys, os
 
-def get_files_with_extension(directory, extension):
-    files = []
-    for name in os.listdir(directory):
-        if name.endswith(extension):
-            files.append(f'{directory}/{name}')
-    return files
+# def get_files_with_extension(directory, extension):
+#     files = []
+#     for name in os.listdir(directory):
+#         if name.endswith(extension):
+#             files.append(f'{directory}/{name}')
+#     return files
 
 
 def read_file(file):
@@ -24,14 +24,14 @@ def write_to_file(file, string, append=False):
         f.write(string)
 
 
-def write_data_to_file(file, data, separator, append=False):
-    if append:
-        mode = 'a'
-    else:
-        mode = 'w'
-    with open(file, mode) as f:
-        for item in data:
-            f.write(f'{item}{separator}')
+# def write_data_to_file(file, data, separator, append=False):
+#     if append:
+#         mode = 'a'
+#     else:
+#         mode = 'w'
+#     with open(file, mode) as f:
+#         for item in data:
+#             f.write(f'{item}{separator}')
 
 
 def input_to_output(input_file):
